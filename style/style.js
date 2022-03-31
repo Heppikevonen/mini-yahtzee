97897792134fbe59a4488d640908e79b19227939
Heppikevonen <hegezu@windowslive.com>
2022-04-01 00:42:16 +0300

    first commit

---
+++ style/style.js
@@ -0,0 +1,88 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: Platform.OS === 'android' ? Constants.statusBarHeight : 0
+  },
+  header: {
+    marginTop: 30,
+    marginBottom: 15,
+    backgroundColor: 'skyblue',
+    flexDirection: 'row',
+  },
+  footer: {
+    marginTop: 20,
+    backgroundColor: 'skyblue',
+    flexDirection: 'row'
+  },
+  title: {
+    color: '#fff',
+    fontWeight: 'bold',
+    flex: 1,
+    fontSize: 23,
+    textAlign: 'center',
+    margin: 10,
+  },
+  author: {
+    color: '#fff',
+    fontWeight: 'bold',
+    flex: 1,
+    fontSize: 15,
+    textAlign: 'center',
+    margin: 10,
+  },
+  gameboard: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  gameinfo: {
+    backgroundColor: '#fff',
+    textAlign: 'center',
+    justifyContent: 'center',
+    fontSize: 20,
+    marginTop: 10
+  },
+  row: {
+    marginTop: 20,
+    padding: 10
+  },
+  flex: {
+    flexDirection: "row"
+  },
+  button: {
+    margin: 30,
+    flexDirection: "row",
+    padding: 10,
+    backgroundColor: "#73CED6",
+    width: 150,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color:"#2B2B52",
+    fontSize: 20
+  },
+  totaltext: {
+    backgroundColor: '#fff',
+    textAlign: 'center',
+    justifyContent: 'center',
+    fontSize: 30,
+    marginTop: 10
+  },
+  bonustext: {
+    backgroundColor: '#fff',
+    textAlign: 'center',
+    justifyContent: 'center',
+    fontSize: 15,
+    marginTop: 10,
+  },
+  pointsrow: {
+    flexDirection: "row",
+    marginTop: 15,
+  },
+});
